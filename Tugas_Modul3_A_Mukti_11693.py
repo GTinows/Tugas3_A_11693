caa888bee6bbb870e672e62568917d9bdb731f59
GTinows <220711693@students.uajy.ac.id>
2024-10-05 22:33:28 +0700

    Update Tugas_Modul3_A_Mukti_11693.py

--- Tugas_Modul3_A_Mukti_11693.py
+++ Tugas_Modul3_A_Mukti_11693.py
@@ -76,7 +76,7 @@ if uploaded_file is not None:
     st.markdown("<h3 style='text-align: center; color:#0073e6;'>Data yang diupload</h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_path = r"C:\Users\mukti\OneDrive\Documents\Data Kuliah\Semester 5\ML\Tugas3_A_11693\GBR_IPK_model.pkl"
+    model_path = r"GBR_IPK_model.pkl"
 
     if os.path.exists(model_path):
         with open(model_path, "rb") as f:
